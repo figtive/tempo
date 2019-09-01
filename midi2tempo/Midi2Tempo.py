617a88d4d8a27625b53f1311df4718858dc9151c
Ilman <muhamad.ilman@ui.ac.id>
2019-09-01 17:17:36 +0700

    refactor write_input method and add new metadata

--- midi2tempo/Midi2Tempo.py
+++ midi2tempo/Midi2Tempo.py
@@ -12,22 +12,25 @@ def main():
     offset = 0 if offset == '' else int(offset)
     write_timestamp = True if input('Include timestamp?(y/n) ').lower() == 'y' else False
     write_empty = True if input('Skip empty?(y/n) ').lower() == 'y' else False
+    checkpoint = int(input('Checkpoints at: '))
 
     output = open('{}.tempo'.format(title), 'w')
     clocks_per_click = get_clocks_per_click(midi_file)
-    write_metadata(output, title, origin, tempo2bpm(get_tempo(midi_file)), get_numerator(midi_file),
-                   get_denominator(midi_file))
-    write_notes(get_notes(midi_file, offset, clocks_per_click), output,
-                get_last_timestamp(midi_file, offset, clocks_per_click), write_empty, write_timestamp)
+    numerator = get_numerator(midi_file)
+    write_metadata(output, title, origin, tempo2bpm(get_tempo(midi_file)), numerator, get_denominator(midi_file),
+                   checkpoint)
+    write_notes(get_notes(midi_file, offset, clocks_per_click, numerator), output,
+                get_last_timestamp(midi_file, offset, clocks_per_click, numerator), write_empty, write_timestamp)
 
     output.close()
 
 
-def write_metadata(file, title, origin, bpm, numerator, denominator):
+def write_metadata(file, title, origin, bpm, numerator, denominator, checkpoint):
     file.write('{}\n'.format(title))
     file.write('{}\n'.format(origin))
     file.write('{}\n'.format(int(round(bpm))))
     file.write('{} {}\n'.format(numerator, denominator))
+    file.write(f'{checkpoint}\n')
 
 
 def get_action(note):
@@ -46,8 +49,8 @@ def get_action(note):
         raise ValueError
 
 
-def get_notes(file, offset, clocks_per_click):
-    timestamp = offset * clocks_per_click * 4
+def get_notes(file, offset, clocks_per_click, numerator):
+    timestamp = offset * clocks_per_click * numerator
     notes = {}
     for track in file.tracks:
         for message in track:
@@ -88,26 +91,26 @@ def get_denominator(file):
                 return message.denominator
 
 
-def write_notes(notes, file, last_timestamp, skip_empty, write_timtestamp):
+def write_notes(notes, file, last_timestamp, skip_empty, write_timestamp):
     for timestamp in range(0, last_timestamp):
         if timestamp in notes.keys():
-            if write_timtestamp:
-                file.write('{} '.format(timestamp))
+            if write_timestamp:
+                file.write(f'{timestamp} ')
             for lane in range(4):
                 if lane in notes[timestamp].keys():
-                    file.write('{}'.format(notes[timestamp][lane])) if lane == 0 \
-                        else file.write(' {}'.format(notes[timestamp][lane]))
+                    note = notes[timestamp][lane]
+                    file.write(f'{note}') if lane == 0 else file.write(f' {note}')
                 else:
                     file.write('.') if lane == 0 else file.write(' .')
             file.write('\n')
         elif not skip_empty:
-            if write_timtestamp:
-                file.write('{} '.format(timestamp))
-            file.write('. . . .\n'.format(timestamp))
+            if write_timestamp:
+                file.write(f'{timestamp} ')
+            file.write('. . . .\n')
 
 
-def get_last_timestamp(file, offset, clocks_per_click):
-    timestamp = offset * 4 * clocks_per_click
+def get_last_timestamp(file, offset, clocks_per_click, numerator):
+    timestamp = offset * numerator * clocks_per_click
     for track in file.tracks:
         for message in track:
             if message.type == 'note_on' or message.type == 'note_off':
